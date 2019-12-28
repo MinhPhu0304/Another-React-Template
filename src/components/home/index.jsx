5c0e658587973d8abc6b5ef3f2ec41c00c3c1467
minh <Lminhphu0304@gmail.com>
2019-12-28 14:18:04 +1300

    add History package for manipulate url path

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 import { store } from 'store'
 import { changeGreetings } from 'store/actions'
@@ -10,6 +11,7 @@ export function Home () {
   return (
     <div>
       <p>Hello from React Redux</p>
+      <Link to="/portal">Click here to navigate to portal page</Link>
     </div>
   )
 }
--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,3 @@ import { rootReducers } from './reducers'
 import { devToolsEnhancer } from 'redux-devtools-extension'
 
 export const store = createStore(rootReducers, devToolsEnhancer())
-
-export function makeStore () {
-  return createStore(rootReducers)
-}
